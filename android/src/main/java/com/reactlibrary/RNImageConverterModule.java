827f50a48912d6ff39217cc9480f40d73bda47cf
ThauEx <github.com@thau.pw>
2019-10-06 17:48:01 +0200

    Use promises for android module

--- android/src/main/java/com/reactlibrary/RNImageConverterModule.java
+++ android/src/main/java/com/reactlibrary/RNImageConverterModule.java
@@ -6,10 +6,10 @@ import android.util.Base64;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 
 public class RNImageConverterModule extends ReactContextBaseJavaModule {
 
@@ -26,22 +26,24 @@ public class RNImageConverterModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void getPNG(String original, Callback success) {
+  public void getPNG(String original, Promise promise) {
     Bitmap bm = BitmapFactory.decodeFile(original);
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     bm.compress(Bitmap.CompressFormat.PNG, 100, baos);
     byte[] byteArrayImage = baos.toByteArray();
     String encodedImage = Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
-    success.invoke(encodedImage);
+
+    promise.resolve(encodedImage);
   }
 
   @ReactMethod
-  public void getJPEG(String original, Callback success) {
+  public void getJPEG(String original, Promise promise) {
     Bitmap bm = BitmapFactory.decodeFile(original);
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
     byte[] byteArrayImage = baos.toByteArray();
     String encodedImage = Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
-    success.invoke(encodedImage);
+
+    promise.resolve(encodedImage);
   }
 }
